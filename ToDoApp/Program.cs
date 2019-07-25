0f4a6af00595f923504bb0998ea59c92ad5e133d
djsc4 <djsc4r@gmail.com>
2019-07-25 17:57:49 -0500

    gradebook

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+
+}
+
+namespace ToDoList___CheckPoint2
+{
+    public class ToDoItem
+    {
+        public int Id { get; private set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime DueDate { get; set; }
+        public ToDoItem(string Description, string Status, DateTime DueDate)
+        {
+            this.Description = Description;
+            this.Status = Status;
+            this.DueDate = DueDate;
+        }
+    }
+}
