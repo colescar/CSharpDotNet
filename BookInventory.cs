1ba5453f0f9afada9477a2eb4317d5437be8505e
djsc4 <djsc4r@gmail.com>
2019-08-13 19:02:17 -0500

    yep

---
+++ BookInventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace bookinventory
+{
+    class Program
+    {
+        static BookContext _context = new BookContext();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Title");
+            string title = Console.ReadLine();
+            Console.WriteLine("Enter the Author");
+            string author = Console.ReadLine();
+            Book toAdd = new Book();
+            toAdd.author = author;
+            toAdd.title = title;
+
+            _context.Add(toAdd);
+
+            IEnumerable<Book> books = _context.books;
+
+
+
+
+
+        }
+        
+
+
+    }
+    
+
+
+}
